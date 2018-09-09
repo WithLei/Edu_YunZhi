d4b7446277eafff92ae5fd10671f8ef62dd37de9
WithLei <syunki@yeah.net>
2018-09-09 08:55:24 +0800

    9.9

--- app/src/main/java/com/android/renly/edu_yunzhi/Common/BaseFragment.java
+++ app/src/main/java/com/android/renly/edu_yunzhi/Common/BaseFragment.java
@@ -9,7 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import butterknife.ButterKnife;
 
 public abstract class BaseFragment extends Fragment {
     private View mContentView;
